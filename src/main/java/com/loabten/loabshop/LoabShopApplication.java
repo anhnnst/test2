655abc09e568a4773bda81d37c2d2fd8a3f9ec39
AnhNNST <anhnnst@yahoo.com>
2018-06-16 14:41:22 +0700

    Init

---
+++ src/main/java/com/loabten/loabshop/LoabShopApplication.java
@@ -0,0 +1,12 @@
+package com.loabten.loabshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoabShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoabShopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/loabten/loabshop/LoabShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loabten.loabshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoabShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
